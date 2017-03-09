820da6479c8a5082cfad98880982ed146357a940
Highflex69 <roland_5_26@hotmail.com>
2017-03-09 13:29:35 +0100

    update

--- src/sample/Model/AddDrawObject.java
+++ src/sample/Model/AddDrawObject.java
@@ -4,7 +4,23 @@ package sample.Model;
  * Created by Teddy on 2017-03-08.
  */
 public class AddDrawObject implements EditCommand{
-    private Object drawObject;
+    private Shape drawObject;
+    private DrawDocument drawDocument;
+
+    public AddDrawObject(DrawDocument drawDocument)
+    {
+        this.drawDocument =drawDocument;
+    }
+
+
+
+    public Shape getDrawObject() {
+        return (Shape) drawObject;
+    }
+    public Shape getDrawObjectClone() {
+        return (Shape) drawObject.clone();
+    }
+
 
     @Override
     public void undo() {
--- src/sample/Model/CircleShape.java
+++ src/sample/Model/CircleShape.java
@@ -1,11 +1,29 @@
 package sample.Model;
 
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 /**
  * Created by Teddy on 2017-03-08.
  */
-public class CircleShape extends Shape {
+public class CircleShape extends Shape{
+    private Circle circle;
+    private double radius;
 
+    public CircleShape() {
+        super();
+        circle = new Circle();
+    }
 
+    public Circle getCircle()
+    {
+        return circle;
+    }
+
+    @Override
+    void draw() {
+
+    }
 }
