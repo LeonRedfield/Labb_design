10bcc99f9d60f322ef1a9c5779464af17847e4e1
Highflex69 <roland_5_26@hotmail.com>
2017-03-09 15:25:43 +0100

    update

--- src/sample/Model/SingleLine.java
+++ src/sample/Model/SingleLine.java
@@ -8,18 +8,50 @@ import javafx.scene.shape.Line;
  */
 public class SingleLine extends Shape{
     Line line;
+    double endX;
+    double endY;
 
     public SingleLine() {
         super();
         line = new Line();
     }
 
-    public Line getLine() {
+    @Override
+    javafx.scene.shape.Shape draw() {
+        line.setStartX(super.x);
+        line.setStartY(super.y);
+        line.setEndX(this.endX);
+        line.setEndY(this.endY);
+        line.setStrokeWidth(super.width);
         return line;
     }
 
-    @Override
-    void draw() {
-
+    public void setStart(double x, double y)
+    {
+        super.x = x;
+        super.y = y;
     }
+
+    public void setEnd(double x, double y)
+    {
+        this.endX = x;
+        this.endY = y;
+    }
+
+    public double getEndX() {
+        return endX;
+    }
+
+    public void setEndX(double endX) {
+        this.endX = endX;
+    }
+
+    public double getEndY() {
+        return endY;
+    }
+
+    public void setEndY(double endY) {
+        this.endY = endY;
+    }
+
 }
