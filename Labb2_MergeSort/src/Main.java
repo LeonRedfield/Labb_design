444e4cfc916e42168438dc39267417db58cade1a
carlitos84 <cgaldo@kth.se>
2017-03-15 21:48:04 +0100

    Merge Sort - Completed

--- Labb2_MergeSort/src/Main.java
+++ Labb2_MergeSort/src/Main.java
@@ -7,12 +7,23 @@ public class Main {
 
     public static void main(String args[]){
 
-        MergeSort ms = new MergeSort(10000000);
-
-        float[] sortedList = ms.sort();
-
-        for(int i = 0; i < sortedList.length-1; i++){
-            if(sortedList[i] > sortedList[i+1]){
+        //MergeSort ms = new MergeSort(10000000);
+        float[] list = new float[100000000];
+        for(int i = 0; i < list.length; i++) {
+            list[i] = new Random().nextInt(100);
+        }
+        //float[] test = new float[]{5, 2, 3, 4, 1, 6, 8, 7, 5, 6, 3, 4, 2, 8, 5, 33, 6, 1, 7};
+        MergeSort2 ms = new MergeSort2(list,0, list.length);
+        System.out.println("Starting...");
+        System.out.println((float)ms.sort()/1000000000);
+        float[] result = ms.result();
+/*
+        for(int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+  */
+        for(int i = 0; i < result.length-1; i++){
+            if(result[i] > result[i+1]){
                 System.out.println("Algorithm error");
             }
         }
---
+++ Labb2_MergeSort/src/MergeSort2.java
@@ -0,0 +1,99 @@
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.RecursiveAction;
+
+/**
+ * Created by Santos on 2017-03-14.
+ */
+public class MergeSort2 extends RecursiveAction{
+
+    private float[] mSource;
+    private int mLength;
+    private int mStart;
+
+    private long startTime;
+    private long stopTime;
+
+
+    public MergeSort2(float[] src, int start, int length) {
+
+        mSource = src;
+        mStart = start;
+        mLength = length;
+    }
+
+    /*
+     *  Returns time it took to complete the calculations
+     */
+    public long sort(){
+        this.startTime = System.nanoTime();
+        this.compute();
+        Arrays.parallelSort(mSource, 0, mLength);
+        this.stopTime = System.nanoTime();
+        return stopTime - startTime;
+
+    }
+
+
+    private int[] sort2(int start, int length) {
+        if(length <= 1){
+            return new int[]{start,length};
+        }
+        int leftSize = length/2;
+        int leftStart = start;
+        int rightSize = length/2;
+        if(length%2==1) {
+            rightSize++;
+        }
+        return merge(sort2(start, leftSize), sort2(start+leftSize, rightSize));
+
+    }
+    private int[] merge(int[] left, int[] right) {
+        int leftSize = left[1];
+        int rightSize = right[1];
+        int totalSize = leftSize + rightSize;
+        float[] list = new float[totalSize];
+        for(int i = 0; i < totalSize; i++){
+            list[i] = mSource[left[0]+i];
+        }
+        int l = 0;
+        int r = 0;
+        for(int i = 0; i < totalSize; i++){
+            if(l < leftSize && (r >= rightSize || list[l] < list[leftSize+r])){
+                mSource[left[0]+l+r] = list[+l];
+                l++;
+            }else
+            {
+                mSource[left[0]+l+r] = list[leftSize+r];
+                r++;
+            }
+
+        }
+        return new int[] {left[0], totalSize};
+    }
+
+    protected static int sThreshold = 511;
+
+    @Override
+    protected void compute() {
+        //System.out.println("compute " +mLength);
+        if(mLength < sThreshold) {
+            return;
+        }
+        else {
+            int leftSplit = mLength / 2;
+            int rightSplit = mLength / 2;
+            //if(mLength%2 == 1)
+                //rightSplit++;
+            MergeSort2 left = new MergeSort2(mSource, mStart, leftSplit);
+            MergeSort2 right = new MergeSort2(mSource, mStart + rightSplit, rightSplit);
+            invokeAll(left, right);
+
+        }
+    }
+
+    public float[] result() {
+        return mSource;
+    }
+}
